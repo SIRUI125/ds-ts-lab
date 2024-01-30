943b6a85804edfe66da3dcc6a1e1a8b7b6d9473e
Sirui Yao <20095250@mail.wit.ie>
2024-01-30 21:06:07 +0000

    Refactor - Add type declaration file; disable redundant console logs.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,13 +1,5 @@
-interface Friend {
-    name: string;
-    phone: string;
-    age: number;
-  }
-  
-  interface ColleagueHistory {
-    current: Colleague[],
-    former: Colleague[]
-  }
+import {Friend, Colleague, ColleagueHistory } from './myTypes'
+
   const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
@@ -20,20 +12,14 @@ interface Friend {
     age: 31,
   };
   
-  const friends: Friend[] = [friend1, friend2];
-  console.log(friends[1]);
+  export const friends = [friend1, friend2];
+  //console.log(friends[1]);
   
   // -------------------
   
   interface ColleagueContact {
     email: string;
     extension: number;
-  }
-  
-  interface Colleague {
-    name: string;
-    department: string;
-    contact: ColleagueContact;
   }
   
   const colleague1: Colleague = {
@@ -68,5 +54,5 @@ interface Friend {
     former: [],
   };
   
-  console.log(colleagues.current[0]);
+  //console.log(colleagues.current[0]);
   
